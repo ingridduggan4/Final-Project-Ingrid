a1977e1610ad994dc43cb93802de91e01be6e0f3
ingridduggan4 <iduggan@tcd.ie>
2022-03-30 16:17:21 +0100

    Added TST class, Queue class and file reader methods in main

--- Vancouver Bus System/Source/BusMain.java
+++ Vancouver Bus System/Source/BusMain.java
@@ -1,21 +1,132 @@
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class BusMain 
 {
-	public static void readInStopsFile(String filename)
+	public static ArrayList<BusStop> readInStopsFile(String filename)
 	{
+		ArrayList<BusStop> busStopList = new ArrayList<BusStop>();
 		
+		try
+    	{
+         FileReader fr = new FileReader(filename);    
+         BufferedReader br = new BufferedReader(fr);
+	        
+	        try
+	        {
+	        	String text=br.readLine();
+	        	
+	            while(text!=null)
+	            {  
+	            	BusStop busStop = new BusStop();
+	            	
+	            	String[] busStopData= text.split(",");
+	            	busStop.iD = Integer.parseInt(busStopData[0]);
+	            	busStop.code = Integer.parseInt(busStopData[1]);
+	            	busStop.name = busStopData[2];
+	            	busStop.desc = busStopData[3];
+	            	busStop.latitude = Double.parseDouble(busStopData[4]);
+	            	busStop.longitude = Double.parseDouble(busStopData[5]);
+	            	busStop.zoneID = busStopData[6];
+	            	busStop.stopURL = Integer.parseInt(busStopData[7]);
+	            	busStop.locationType = Integer.parseInt(busStopData[8]);
+	            	busStop.parentStation = busStopData[9];
+	            	
+	                busStopList.add(busStop);
+	                text=br.readLine();
+	            }
+	
+	            br.close();    
+	            fr.close();
+	            
+	         }catch(Exception e){;}
+        
+    	}catch(Exception e){;}
+		
+		return busStopList;
 	}
 	
-	public static void readInTransfersFile(String filename)
+	public static ArrayList<Transfers> readInTransfersFile(String filename)
 	{
+		ArrayList<Transfers> transfersList = new ArrayList<Transfers>();
 		
+		try
+    	{
+         FileReader fr = new FileReader(filename);    
+         BufferedReader br = new BufferedReader(fr);
+	        
+	        try
+	        {
+	        	String text=br.readLine();
+	        	
+	            while(text!=null)
+	            {  
+	            	Transfers transfer = new Transfers();
+	            	
+	            	String[] transferData= text.split(",");
+	            	transfer.fromStopID = Integer.parseInt(transferData[0]);
+	            	transfer.toStopID = Integer.parseInt(transferData[1]);
+	            	transfer.transferType = Integer.parseInt(transferData[2]);
+	            	transfer.minTransferTime = Integer.parseInt(transferData[3]);
+	            	
+	                transfersList.add(transfer);
+	                text=br.readLine();
+	            }
+	
+	            br.close();    
+	            fr.close();
+	            
+	         }catch(Exception e){;}
+        
+    	}catch(Exception e){;}
+		
+		return transfersList;
+    	
 	}
 	
-	public static void readInStopTimesFile(String filename)
+	public static ArrayList<StopTimes> readInStopTimesFile(String filename)
 	{
+		ArrayList<StopTimes> stopTimesList = new ArrayList<StopTimes>();
 		
+		try
+    	{
+         FileReader fr = new FileReader(filename);    
+         BufferedReader br = new BufferedReader(fr);
+	        
+	        try
+	        {
+	        	String text=br.readLine();
+	        	
+	            while(text!=null)
+	            {  
+	            	StopTimes stopTime = new StopTimes();
+	            	
+	            	String[] stopTimeData= text.split(",");
+	            	stopTime.tripID = Integer.parseInt(stopTimeData[0]);
+	            	stopTime.arrivalTime = stopTimeData[1];
+	            	stopTime.departureTime = stopTimeData[2];
+	            	stopTime.stopID = Integer.parseInt(stopTimeData[3]);
+	            	stopTime.stopSequence = Integer.parseInt(stopTimeData[4]);
+	            	stopTime.stopHeadsign = Integer.parseInt(stopTimeData[5]);
+	            	stopTime.pickupType = Integer.parseInt(stopTimeData[6]);
+	            	stopTime.dropoffType = Integer.parseInt(stopTimeData[7]);
+	            	stopTime.distTraveled = Double.parseDouble(stopTimeData[3]);
+	            	
+	                stopTimesList.add(stopTime);
+	                text=br.readLine();
+	            }
+	
+	            br.close();    
+	            fr.close();
+	            
+	         }catch(Exception e){;}
+        
+    	}catch(Exception e){;}
+		
+		return stopTimesList;
 	}
 
 	public static void main(String[] args) 
@@ -28,6 +139,8 @@ public class BusMain
 		System.out.println("(Please enter either 1,2,3 or 'quit')");
 		Scanner input = new Scanner(System.in);
 		String userInput = input.next();
+		
+		readInTransfersFile("Transfers");
 		
 		while(!userInput.equalsIgnoreCase("quit"))
 		{
@@ -43,7 +156,8 @@ public class BusMain
 				System.out.println("Error: invalid input");
 
 			}
-			else if(userInput.equals("1"))
+			
+			if(userInput.equals("1"))
 			{
 				System.out.print("ONE");
 			}
